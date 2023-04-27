3fb42c3ccf900faad6e635c4157dea3bce9297d8
Sarkhan <david.jonquieres@gmail.com>
2023-04-27 09:54:27 +0200

    Feat : create todo repository

--- src/main/java/com/sogeti/todos/services/TodoServiceImpl.java
+++ src/main/java/com/sogeti/todos/services/TodoServiceImpl.java
@@ -1,4 +1,77 @@
 package com.sogeti.todos.services;
 
-public class TodoServiceImpl {
+import com.sogeti.todos.entities.Todo;
+import com.sogeti.todos.exceptions.TodoNotFoundException;
+import com.sogeti.todos.repositories.TodoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoServiceImpl implements TodoService{
+    private final TodoRepository todoRepository;
+
+    public TodoServiceImpl(TodoRepository todoRepository) { this.todoRepository = todoRepository; }
+
+    /**
+     * Lister toutes les todos
+     */
+    @Override
+    public List<Todo> getAllTodos() { return todoRepository.findAll(); }
+
+    /**
+     * Obtenir une to-do via son ID
+     */
+    @Override
+    public Todo getTodoById(Long todoId) throws TodoNotFoundException {
+        Todo currentTodo = todoRepository.findById(todoId).orElseThrow(() -> new TodoNotFoundException("Todo not found with this ID"));
+        return currentTodo;
+    }
+
+    /**
+     * Ajouter une to-do
+     */
+    @Override
+    public Todo addTodo(@NotNull Todo todo) {
+        Todo currentTodo = todo;
+        return todoRepository.save(currentTodo);
+    }
+
+    /**
+     * Permet de cahnger le status done d'une to-do
+     */
+    @Override
+    @Transactional
+    public Todo changeDoneState(long todoId) throws TodoNotFoundException {
+        Todo currentTodo = todoRepository.findById(todoId).orElseThrow(() -> new TodoNotFoundException("Todo not found with this ID"));
+        currentTodo.setDone(!currentTodo.isDone());
+        return currentTodo;
+    }
+
+    /**
+     * Supprimer une To-do
+     */
+    @Override
+    public Optional<Todo> deleteTodo(Long todoId) throws TodoNotFoundException {
+        Optional<Todo> deletedTodo = todoRepository.findById(todoId);
+        if (deletedTodo.isEmpty()) {
+            throw new TodoNotFoundException("This Todo doesn't exist");
+        }
+        todoRepository.deleteById(todoId);
+        return deletedTodo;
+    }
+
+    /**
+     * Permet d'éditer le titre et la description d'un to-do
+     */
+    @Override
+    public Todo editTodo(Long todoId, String newTitle, String newDescription) throws TodoNotFoundException {
+        Todo todo = todoRepository.findById(todoId).orElseThrow(() -> new TodoNotFoundException("Todo not found with this ID"));
+        todo.setTitle(newTitle);
+        todo.setDescription(newDescription);
+        return todoRepository.save(todo);
+    }
 }
